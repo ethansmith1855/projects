766cad4f29a4ff3d6594e06697e77f1b8afd2e08
ethansmith1855 <89827161+ethansmith1855@users.noreply.github.com>
2021-08-31 23:11:36 +0000

    Asking user if they are doing well

--- replit_test/helloworld.cs
+++ replit_test/helloworld.cs
@@ -4,4 +4,16 @@ Console.WriteLine("Whats Your Name?");
 string name = Console.ReadLine();
 Console.Clear();
 Console.WriteLine($"Hello {name}");
+Console.ReadKey();
+Console.Clear();
+Console.WriteLine("How are you doing?");
+string wellness = Console.ReadLine();
+Console.Clear();
+if (wellness == "good"){
+  Console.WriteLine("Im glad to hear that " + name);
+}
+if (wellness == "bad"){
+  Console.WriteLine("Im sorry to hear that " + name);
+}
+Console.ReadKey();
 
