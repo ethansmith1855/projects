a85a1c9169ebf44a7ddba3aa4120c03f2589eb5d
ethansmith1855 <ethansmith1855@gmail.com>
2021-08-31 21:25:19 -0700

    Adding Keyword application c#

---
+++ KeyWord/KeyWord/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyWord
+{
+    class Program
+    {
+
+        public static List<string> keyWordList = new List<string>();
+
+        static void Main(string[] args)
+        {
+            AskForKeyWords();
+            Clear();
+            Console.WriteLine("What would you like to scan?");   // This is what im checking for keywords in
+            string input = Console.ReadLine();
+            SearchForKeyWord(input);
+        }
+
+        static void AskForKeyWords()
+        {
+            Console.WriteLine("What key words are you looking for?");
+            Console.WriteLine("Use spaces to separate them.");
+            Console.WriteLine();
+            string keyWords = Console.ReadLine();
+            keyWords += " ";    //Adds a space for last word
+            string word = "";
+            foreach (var letter in keyWords)
+            {
+                if (letter != ' ')
+                {
+                    word += letter;
+                }
+                else
+                {
+                    keyWordList.Add(word);
+                    word = "";
+                }
+            }
+        }
+
+        static void WriteWord(string word)
+        {
+            bool keyWordFound = false;
+            foreach (var keyWord in keyWordList)
+            {
+                keyWordFound = false;
+
+                if (word == keyWord)
+                {
+                    keyWordFound = true;
+                    break;
+                }
+                else
+                {
+                    keyWordFound = false;
+                }
+            }
+            if (keyWordFound == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(word);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.Write(word);
+            }
+            Console.Write(" ");
+        }
+
+        static void SearchForKeyWord(string input)
+        {
+            Clear();
+            Console.WriteLine("\n\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            string word = "";
+            input += " ";       // for last word
+            foreach (var letter in input)
+            {
+                if (letter != ' ')
+                {
+                    word += letter;
+                }
+                else
+                {
+                    WriteWord(word);
+                    word = "";
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void Clear()
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+        }
+
+    }
+}
